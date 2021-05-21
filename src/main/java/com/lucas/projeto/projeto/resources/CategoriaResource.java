1776137ed66eaa3998bbf8f5cd2e3124472630ac
Lucas Rodrigues <43002509+lucasrsv@users.noreply.github.com>
2021-05-21 13:44:46 -0300

    Create PUT endpoint for Categoria

--- src/main/java/com/lucas/projeto/projeto/resources/CategoriaResource.java
+++ src/main/java/com/lucas/projeto/projeto/resources/CategoriaResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -27,8 +28,8 @@ public class CategoriaResource {
     private CategoriasService service;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id) {
-        Categoria obj = service.buscar(id);
+    public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+        Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
 
@@ -38,4 +39,12 @@ public class CategoriaResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 }
