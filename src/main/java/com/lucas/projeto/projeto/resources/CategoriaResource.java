fa7001621bc4e011832c6af809f34b35aba07e64
Lucas Rodrigues <43002509+lucasrsv@users.noreply.github.com>
2021-05-21 13:17:06 -0300

    Create a POST endpoint for Categoria

--- src/main/java/com/lucas/projeto/projeto/resources/CategoriaResource.java
+++ src/main/java/com/lucas/projeto/projeto/resources/CategoriaResource.java
@@ -1,28 +1,41 @@
 package com.lucas.projeto.projeto.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.Servlet;
 
 import com.lucas.projeto.projeto.domain.Categoria;
 import com.lucas.projeto.projeto.services.CategoriasService;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @Autowired
-    private CategoriasService categoriasService;
+    private CategoriasService service;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id) {
-        Categoria obj = categoriasService.buscar(id);
+        Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
